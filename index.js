6671822972e3b19b648b3f69453340e333e44e8d
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-09-17 18:04:59 +0200

    Remove --return

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ try {
 if(os.platform() === 'darwin') {
   bsb = `script -q /dev/null ${bsb} -make-world -color`
 } else {
-  bsb = `script --return -qfc ${bsb} -make-world -color /dev/null`
+  bsb = `script -qfc ${bsb} -make-world -color /dev/null`
 }
 
 const outputDir = 'lib'
